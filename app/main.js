fb4d6c1290352b602869785974bb9222bbcde3c6
meilani <lauraho2006@gmail.com>
2024-10-06 08:43:59 -0400

    http request

--- app/main.js
+++ app/main.js
@@ -5,11 +5,27 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write(
-        'HTTP/1.1 200 OK\r\n\r\n' 
-    );
+
+
+
+    socket.on('data', data => {
+        let path = data.split(' ')[1]
+        if (path === '/') {
+            socket.write(
+                'HTTP/1.1 200 OK\r\n\r\n' 
+            );
+        } else {
+            socket.write(
+                'HTTP/1.1 404 Not Found\r\n\r\n'
+            );
+        }
+    });
+
+    socket.setEncoding('utf8')
+
     socket.on("close", () => {
         socket.end();
+        socket.close();
     });
 });
 
