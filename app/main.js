b32553952b834cbce6e723a30f3924c5f328efe3
meilani <lauraho2006@gmail.com>
2024-10-06 13:33:38 -0400

    return a file

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require("fs");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -11,14 +12,26 @@ const server = net.createServer((socket) => {
         let req = dataArr[0].split(' ')
         let path = req[1]
         const dataObj = {}
+        console.log(dataObj)
         for (let i = 1; i < dataArr.length; i++) {
             let newProp = dataArr[i].split(':')
             if (newProp.length == 2) {
                 dataObj[newProp[0]] = newProp[1].trim()
             }
         }
-
-        if (path.startsWith('/echo/')) {
+        if (path.startsWith('/files/')) {
+            let fileName = `/tmp/${path.slice(7)}`
+            try {
+                const stats = fs.statSync(fileName);
+                let resBody = fs.readFile(fileName, 'utf8', function(err, data) {
+                    socket.write(
+                        `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${stats.size}\r\n\r\n${data}`
+                    );
+                })
+            } catch (err) {
+                console.error(err);
+            }
+        } else if (path.startsWith('/echo/')) {
             let resBody = path.slice(6);
             let contentLength = resBody.length;
             socket.write(
